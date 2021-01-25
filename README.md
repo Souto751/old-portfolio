ade5aff1fc8187f5cf99631de2af2f458344e086
Souto751 <agus.souto75@gmail.com>
2021-01-25 11:23:03 -0300

    Update 25/01/21

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio
+
+This is my portfolio, made with HTML, CSS and JavaScript.
+It isn't finished yet, so there will be some updates on it.
---
+++ script/index.js
@@ -0,0 +1,88 @@
+// OPENS WELCOME SECTION & CLOSES PROJECTS SECTION
+function toWelcome()
+{
+    let projects = document.getElementById('projects-section');
+    projects.style.display = 'none';
+
+    let welcome = document.getElementById('welcome-section');
+    welcome.style.display = 'initial';
+
+    let under = document.getElementById('btn-welcome');
+    let noUnder = document.getElementById('btn-projects');
+
+    under.style.textDecoration = 'underline';
+    under.style.textDecorationColor = '#a00';
+    noUnder.style.textDecoration = 'none';
+}
+
+// OPENS PROJECTS SECTION & CLOSES WELCOME SECTION
+function toProjects()
+{
+    let welcome = document.getElementById('welcome-section');
+    welcome.style.display = 'none';
+
+    let projects = document.getElementById('projects-section');
+    projects.style.display = 'flex';
+
+    let under = document.getElementById('btn-projects');
+    let noUnder = document.getElementById('btn-welcome');
+
+    under.style.textDecoration = 'underline';
+    under.style.textDecorationColor = '#a00';
+    noUnder.style.textDecoration = 'none';
+}
+
+// OPENS SKILLS WINDOW & CLOSES MAIL WINDOW
+function skillsWindow(state)
+{
+    let skills = document.getElementById('skills-section');
+    skills.style.display = state;
+
+    let under = document.getElementById('btn-skills');
+
+    if(state == 'none')
+    {
+        under.style.textShadow = 'none';
+    }
+    else
+    {
+        mail('none');
+        under.style.textShadow = '5px 5px 10px #000';
+    }
+}
+
+// AUTO SLIDES
+let skillCounter = 2;
+let toolCounter = 2;
+
+function autoSlideSkills(){
+    document.getElementById('radio' + skillCounter).checked = true;
+    skillCounter++;
+    if(skillCounter > 6){
+        skillCounter = 1;
+    }
+}
+
+function autoSlideTools(){
+    document.getElementById('tool' + toolCounter).checked = true;
+    toolCounter++;
+    if(toolCounter > 2){
+        toolCounter = 1;
+    }
+}
+
+// SET INTERVALS FOR AUTO SLIDES
+setInterval(autoSlideSkills, 5000);
+setInterval(autoSlideTools, 5000);
+
+// OPENS MAIL WINDOW & CLOSES SKILLS WINDOW
+function mail(state)
+{
+    let mail = document.getElementById('mail-window');
+    mail.style.display = state;
+    
+    if(state != 'none')
+    {
+        skillsWindow('none');
+    }
+}
